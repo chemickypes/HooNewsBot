bf7660c70fea39b9a2511ecdfa7a9d62ccd573ef
angelo <mor.angelo.mor@gmail.com>
2021-01-27 15:05:37 +0100

    category and language

--- hoonewsbot.py
+++ hoonewsbot.py
@@ -2,17 +2,31 @@ import feedparser
 import requests
 from rx import of, operators as op
 
-categories = {'TECH': (
-    'Technology', 1, 'https://news.google.com/news/rss/headlines/section/topic/TECHNOLOGY?ned=IT&hl=IT'),
-    'GENERAL': ('General', 0, 'https://news.google.com/news/rss/?gl=IN&ned=IT&hl=IT')}
+# https://news.google.com/news/rss/headlines/section/topic/CATEGORYNAME?ned=in&hl=en
+rss_url_struct = "https://news.google.com/news/rss/headlines/section/topic/{}?{}"
+general_rss_structure = "https://news.google.com/news/rss/?{}"
+
+categories = ["WORLD", "NATION", "BUSINESS", "TECHNOLOGY", "ENTERTAINMENT", "SPORTS", "SCIENCE", "HEALTH", "GENERAL"]
 
 
 def resolve_link(link):
     return requests.get(link).url
 
 
-def list_of_news(type_of_news):
-    news_feed = feedparser.parse(categories[type_of_news][2])
+def __find_lang(lang):
+    return f'ned={lang}&hl={lang}'
+
+
+def get_categories(lang):
+    return list(map(lambda cat: cat.lower().capitalize(), categories))
+
+
+def list_of_news(type_of_news, lang):
+    url = rss_url_struct.format(type_of_news,
+                                __find_lang(lang)) if type_of_news != "GENERAL" else general_rss_structure.format(
+        __find_lang(lang))
+    print(url)
+    news_feed = feedparser.parse(url)
     source = of(*news_feed['entries'][:15]).pipe(
         op.map(lambda entry: f'{entry["title"]}\n{resolve_link(entry["link"])}')
     )
--- main.py
+++ main.py
@@ -10,32 +10,36 @@ bot = telebot.TeleBot(secrets.BOT_TOKEN)
 
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
+    print(message)
     bot.reply_to(message, """\
 Hi you can read news.
 type /read to choose the category
 """)
 
 
-def gen_markup(chat_id):
+def gen_markup(chat_id, lang):
     markup = InlineKeyboardMarkup()
-    markup.row_width = len(categories)
-    for key, cat in categories.items():
-        markup.add(InlineKeyboardButton(cat[0], callback_data=f"{key}:{chat_id}"))
+    markup.row_width = 2
+    for cat in hoonewsbot.get_categories(lang):
+        markup.add(InlineKeyboardButton(cat, callback_data=f"{cat}:{chat_id}"))
     return markup
 
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_query(call):
+    print(call)
     data = call.data.split(':')
     bot.send_message(data[1], "Attendere...")
-    hoonewsbot.list_of_news(data[0]).subscribe(
+    hoonewsbot.list_of_news(data[0].upper(), call.from_user.language_code).subscribe(
         lambda value: bot.send_message(data[1], value)
     )
 
 
 @bot.message_handler(commands=['read'])
 def read(message):
-    bot.reply_to(message, 'Choose Category', reply_markup=gen_markup(message.chat.id))
+    print(message)
+    bot.reply_to(message, 'Choose Category',
+                 reply_markup=gen_markup(message.chat.id, message.from_user.language_code))
 
 
 def start_polling():
