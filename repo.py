8a1a264062eadd90aaa9ca32af0a2672cf52ffe4
angelo <mor.angelo.mor@gmail.com>
2021-01-29 18:05:49 +0100

    improved repo

--- repo.py
+++ repo.py
@@ -2,14 +2,10 @@ import write_data
 import feedparser
 from datetime import datetime
 from time import mktime
-from rx.subject import Subject
-from models import HooNewsMessage
 import requests
 import pycountry
 
 db = write_data.db
-
-message_subject = Subject()
 
 
 def __resolve_link(link):
@@ -36,11 +32,6 @@ def get_user(chat_id):
 def get_categories(chat_id):
     return [(cat.capitalize(), index) for index, cat in
             enumerate(db.collection('feeds').document('categories').to_dict()['categories'])]
-
-
-def make_search_simple(chat_it, category):
-    user = get_user(chat_it)
-    make_search(chat_it, user['language'], category, user['country'])
 
 
 def needs_new_feed(chat_id):
@@ -84,40 +75,6 @@ def get_articles(chat_id, category, lang, country):
     return True
 
 
-def make_search(chat_id, lang, category, nation):
-    countries = db.collection('feeds').document('countries').get().to_dict()
-    if lang not in countries['lang'] or nation not in countries['countries']:
-        # populate feeds because there is not the country or the lang or both
-        message_subject.on_next(HooNewsMessage('ALERT', (chat_id, 'FEEDS_LOADING')))
-        write_data.write_generic_feeds(lang, nation)
-        if lang not in countries['lang']: countries['lang'].append(lang)
-        if nation not in countries['countries']: countries['countries'].append(nation)
-        db.collection('feeds').document('countries').set(countries, merge=True)
-
-    message_subject.on_next(HooNewsMessage('LOADING', (chat_id, 'NEWS_LOADING')))
-    feeds = db.collection('feeds').where('category', '==', category) \
-        .where('language', '==', lang) \
-        .where('country', '==', nation).stream()
-
-    list_of_articles = []
-
-    for feed in feeds:
-        ll = feedparser.parse(feed.to_dict()['link'])
-        list_of_articles.extend(
-            [{'id': entry['id'], 'title': entry['title'], 'link': entry['link'],
-              'timestamp_parsed': entry['published_parsed']} for entry in ll['entries']]
-        )
-
-    list_of_articles.sort(key=lambda el: datetime.fromtimestamp(mktime(el['timestamp_parsed'])), reverse=True)
-    print(len(list_of_articles))
-
-    for index, element in enumerate(list_of_articles[:30]):
-        db.collection('live_search').document(str(chat_id)).collection('articles').document(str(index)).set(element,
-                                                                                                            merge=True)
-
-    get_article(chat_id, '0')
-
-
 def get_article(chat_id, article_id):
     art_doc = db.collection('live_search').document(str(chat_id)).collection('articles').document(
         article_id).get()
@@ -135,8 +92,8 @@ def get_article(chat_id, article_id):
 
 
 if __name__ == '__main__':
-    message_subject.subscribe(lambda item: print(item))
-    make_search(6483, 'en', 'technology', 'ca')
+    #  message_subject.subscribe(lambda item: print(item))
+    #  make_search(6483, 'en', 'technology', 'ca')
     get_article(6483, '1')
     get_article(6483, '2')
     get_article(6483, '3')
