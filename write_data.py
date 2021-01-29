422c3fd06961a67c7773ff444a32debaea82e321
angelo <mor.angelo.mor@gmail.com>
2021-01-29 17:15:38 +0100

    make search

--- write_data.py
+++ write_data.py
@@ -2,6 +2,9 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+import pycountry
+from pycountry import languages
+
 
 ultimo_uomo_url = "https://www.ultimouomo.com/rss"
 g_news = 'https://news.google.com/news/rss/headlines/section/topic/TECHNOLOGY?ned=it&hl=it'
@@ -38,49 +41,4 @@ def write_generic_feeds(lang, country):
 
 
 if __name__ == '__main__':
-    '''news_feed_ul = feedparser.parse(ultimo_uomo_url)
-    news_feed_gn = feedparser.parse(wired_news)
-
-    for entry in [*news_feed_ul['entries'], *news_feed_gn['entries']]:
-        print(entry['id'], json.dumps(entry))'''
-    '''for i, cat in categories.items():
-        db.collection('feeds').document().set({
-            'link': rss_url_struct.format(categories[i],
-                                          __find_lang('it', 'it')) if i != '8' else general_rss_structure.format(
-                __find_lang('it', 'it')),
-            'category': cat.lower(),
-            'language': 'it',
-            'country': 'it'
-        })
-        db.collection('feeds').document().set({
-            'link': rss_url_struct.format(categories[i],
-                                          __find_lang('en', 'us')) if i != '8' else general_rss_structure.format(
-                __find_lang('en', 'us')),
-            'category': cat.lower(),
-            'language': 'en',
-            'country': 'us'
-        })
-        db.collection('feeds').document().set({
-            'link': rss_url_struct.format(categories[i],
-                                          __find_lang('en', 'uk')) if i != '8' else general_rss_structure.format(
-                __find_lang('en', 'uk')),
-            'category': cat.lower(),
-            'language': 'en',
-            'country': 'uk'
-        })
-    db.collection('feeds').document().set({
-        'link': 'https://www.wired.it/feed/',
-        'category': categories['3'].lower(),
-        'language': 'it',
-        'country': 'it'
-    })
-    db.collection('feeds').document().set({
-        'link': ultimo_uomo_url,
-        'category': categories['5'].lower(),
-        'language': 'it',
-        'country': 'it'
-    })'''
-    db.collection('feeds').document('countries').set({
-        'countries': ['it', 'uk', 'us'],
-        'lang': ['it', 'en']
-    })
+    print(pycountry.countries.search_fuzzy('swiz'))
