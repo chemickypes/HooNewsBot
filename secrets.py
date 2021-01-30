37db0c11f523ebc62b9a0be918a11a0c53bf70f0
angelo <mor.angelo.mor@gmail.com>
2021-01-30 19:42:40 +0100

    secrets file

--- secrets.py
+++ secrets.py
@@ -1,2 +1,7 @@
-BOT_TOKEN = "1639276073:AAH1adwjgnPiREAYCX7iW10V5cNvRpCnr84"
+BOT_TOKEN = '1639276073:AAH1adwjgnPiREAYCX7iW10V5cNvRpCnr84'
+DEBUG_BOT_TOKEN = '1648537217:AAHLsKxDtC2qZ_GVgBUmJjVH35JjIRZcqtE'
 HEROKU_URL = 'https://hoonewsbot.herokuapp.com/'
+
+
+def get_token(debug):
+    return DEBUG_BOT_TOKEN if debug else BOT_TOKEN
